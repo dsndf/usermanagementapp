e8116c59932120abd974ada8dd605858f335fa2d
gaurav jain <gjain8883@gmail.com>
2024-10-09 23:18:07 +0530

    changes done

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,24 +5,26 @@ import { clearSession } from "../../lib/helper";
 import toast from "react-hot-toast";
 
 const Header = () => {
-  const { setAuthToken } = useContext(Context);
+  const { setAuthToken, authToken } = useContext(Context);
 
   const logoutHandler = () => {
     clearSession();
     setAuthToken(null);
-    toast.success("Logged out")
+    toast.success("Logged out");
   };
 
   return (
     <div className="p-6 h-16 flex justify-between items-center border shadow-md shadow-blue-100">
       <Logo />
       <ul>
-        <button
-          onClick={logoutHandler}
-          className="py-2 px-4 bg-blue-500 text-white rounded-md"
-        >
-          Logout
-        </button>
+        {authToken && (
+          <button
+            onClick={logoutHandler}
+            className="py-2 px-4 bg-blue-500 text-white rounded-md"
+          >
+            Logout
+          </button>
+        )}
       </ul>
     </div>
   );
